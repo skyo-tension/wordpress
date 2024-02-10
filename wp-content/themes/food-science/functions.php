e38d0beecd916c1a4c2d668991289af035029506
skyo-tension <chenglong261@yahoo.co.jp>
2024-02-10 11:16:39 +0900

    追加：WEBサイト拡張する
    ・コメント追加
    ・独自固定ページ作成
    ・パスワード保護されたページ作成

--- wp-content/themes/food-science/functions.php
+++ wp-content/themes/food-science/functions.php
@@ -32,49 +32,49 @@ function my_wpcf7_autop(){
     return false;
 }
 
-// /**
-//  * メインクエリを変更する
-//  */
-// add_action('pre_get_posts', 'my_pre_get_posts');
-// function my_pre_get_posts($query)
-// {
-//     // 管理画面、メインクエリ以外には設定しない
-//     if (is_admin() || !$query->is_main_query()) {
-//         return;
-//     }
-//     // トップページの場合
-//     if ($query->is_home()) {
-//         $query->set('posts_per_page', 3);
-//         return;
-//     }
-// }
+/**
+ * メインクエリを変更する
+ */
+add_action('pre_get_posts', 'my_pre_get_posts');
+function my_pre_get_posts($query)
+{
+    // 管理画面、メインクエリ以外には設定しない
+    if (is_admin() || !$query->is_main_query()) {
+        return;
+    }
+    // トップページの場合
+    if ($query->is_home()) {
+        $query->set('posts_per_page', 3);
+        return;
+    }
+}
 
-// /**
-//  * タイトルの「保護中」の文字を削除する
-//  */
-// add_filter('protected_title_format', 'my_protected_title');
-// function my_protected_title($title)
-// {
-//     return '%s';
-// }
+/**
+ * タイトルの「保護中」の文字を削除する
+ */
+add_filter('protected_title_format', 'my_protected_title');
+function my_protected_title($title)
+{
+    return '%s';
+}
 
-// /**
-//  * パスワード保護フォームをカスタマイズする
-//  */
-// add_filter('the_password_form', 'my_password_form');
-// function my_password_form()
-// {
-//     remove_filter('the_content', 'wpautop');
-//     $wp_login_url = wp_login_url();
-//     $html = <<<HTML
-// <p>パスワードを入力してください。<p>
-// <form class="post-password-form" action="{$wp_login_url}?action=postpass" method="post">
-// <input name="post_password" type="password" />
-// <input type="submit" name="送信" value="送信" />
-// </form>
-// HTML;
-//     return $html;
-// }
+/**
+ * パスワード保護フォームをカスタマイズする
+ */
+add_filter('the_password_form', 'my_password_form');
+function my_password_form()
+{
+    remove_filter('the_content', 'wpautop');
+    $wp_login_url = wp_login_url();
+    $html = <<<HTML
+<p>パスワードを入力してください。<p>
+<form class="post-password-form" action="{$wp_login_url}?action=postpass" method="post">
+<input name="post_password" type="password" />
+<input type="submit" name="送信" value="送信" />
+</form>
+HTML;
+    return $html;
+}
 
 // /**
 //  * ブロックエディターにCSSを読み込む
